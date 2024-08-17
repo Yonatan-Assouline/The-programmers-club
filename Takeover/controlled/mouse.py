b556fc56d47c6dd6a8a315649955517242b88445
Yonatan-Assouline <yonatan6i5i@gmail.com>
2024-08-17 19:23:08 +0300

    mouse.py

--- Takeover/controlled/mouse.py
+++ Takeover/controlled/mouse.py
@@ -1,5 +1,8 @@
-from pynput import mouse
+from pynput.mouse import Button, Controller
 import socket 
+import ast
+
+mouse = Controller()
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 port = 5050
@@ -8,6 +11,14 @@ s.connect(('10.100.102.39', port))
 while True:
     try:
         data=s.recv(1024).decode()
+        data_tuple = ast.literal_eval(data)
+        x, y = data_tuple[1]
+        if data_tuple[0]=='1':   
+            mouse.position = (x, y)
+        elif data_tuple[0]=='2':
+            mouse.click(Button.left)
+        else:
+            mouse.click(Button.right)
     except:
         break
         
